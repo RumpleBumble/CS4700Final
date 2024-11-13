97fd4ea17167e2802baabe017dbbda8e9921abde
rcorv20 <164258114+rcorv20@users.noreply.github.com>
2024-11-13 15:08:17 -0800

    Add files via upload

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 5f;
+    private bool isGrounded;
+    private Rigidbody rb;
+
+    // Gravity Mode variables
+    private bool isLowGravity = true;
+    public float lowGravityJumpForce = 7f;
+    public float highGravityJumpForce = 3f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+        if (Input.GetKeyDown(KeyCode.G)) // Press 'G' to toggle gravity mode
+        {
+            ToggleGravityMode();
+        }
+    }
+
+    void MovePlayer()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.MovePosition(transform.position + movement * speed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        float currentJumpForce = isLowGravity ? lowGravityJumpForce : highGravityJumpForce;
+        rb.AddForce(Vector3.up * currentJumpForce, ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+    void ToggleGravityMode()
+    {
+        isLowGravity = !isLowGravity;
+        Debug.Log("Gravity Mode: " + (isLowGravity ? "Low Gravity" : "High Gravity"));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
