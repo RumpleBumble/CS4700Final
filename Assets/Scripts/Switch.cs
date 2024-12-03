655119b239d982e969dca55766dde6810aae8599
Temporium1 <jacquesmai123@gmail.com>
2024-12-02 21:28:55 -0800

    added proximity prompt to lever and gravity indicator to switch

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Switch : MonoBehaviour
 {
     public float lowGravity = 4.9f;   
     public float highGravity = 19.6f;
     private bool playerInRange = false;
-    
+
+    public Canvas canvas;
+
+    public TMP_Text gravityText;
+
+    [SerializeField] private AudioClip lever;
+
     private void Update()
     {
+
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
             Debug.Log($"E pressed - Current gravity: {Physics.gravity.y}");
+            SFXManager.instance.PlaySFXClip(lever, transform, 0.5f);
             GameManager.Instance.ToggleGravityMode();
             float newGravity = GameManager.Instance.isLowGravity ? lowGravity : highGravity;
             Physics.gravity = new Vector3(0, -newGravity, 0);
             Debug.Log($"Gravity changed to: {Physics.gravity.y}");
+            gravityText.text = $"Current gravity: {Physics.gravity.y}";
         }
     }
 
@@ -24,6 +35,7 @@ public class Switch : MonoBehaviour
         {
             playerInRange = true;
             Debug.Log("Player entered trigger zone");
+            canvas.enabled = true;
         }
     }
 
@@ -33,6 +45,7 @@ public class Switch : MonoBehaviour
         {
             playerInRange = false;
             Debug.Log("Player exited trigger zone");
+            canvas.enabled = false;
         }
     }
 }
